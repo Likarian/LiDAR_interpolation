28131a8336e1f927e3d96f82980b870a4a1b2ce8
Wonjik Kim <44762643+Likarian@users.noreply.github.com>
2021-11-05 15:44:52 +0900

    Update DownloadAndUnzip.sh

--- DownloadAndUnzip.sh
+++ DownloadAndUnzip.sh
@@ -1,3 +1,3 @@
-wget www.ok.ctrl.titech.ac.jp/res/PDY/LiDAR_Interpolation.zip
+wget www.ok.ctrl.titech.ac.jp/res/DSR/PDY/LiDAR_Interpolation.zip
 unzip LiDAR_Interpolation.zip
 rm LiDAR_Interpolation.zip
